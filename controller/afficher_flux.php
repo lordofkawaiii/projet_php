e5e65ec4d33391b7bff364b6bde081d5fa8f1429
lordofkawaiii <thierry.duchaine@gmail.com>
2017-10-24 16:29:46 +0200

    ajout dossiers vue et controller

--- controller/afficher_flux.php
+++ controller/afficher_flux.php
@@ -1,3 +1,8 @@
 <?php
-
+global $out;
+$dao = new DAO ();
+$i = 0;
+foreach ( $dao->readAllUrlFromRSS () as $url ) {
+	$out [i] = $url;
+}
 ?>
---
+++ vue/afficher_flux_view.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'controller/afficher_flux.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>afficher flux</title>
+<style type="text/css">
+</style>
+</head>
+<body>
+<form action= ".<?php
+$_SERVER ['PHP_SELF']?>. " > ";
+voici la liste des flux:
+</form>
