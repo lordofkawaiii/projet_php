78f1098ff6d66da81659f100636f78aaad6f03fa
ChatonRêveur <vincent.marechal.pro@gmail.com>
2017-10-30 10:44:36 +0100

    Debut des vues + vues pour les erreurs

---
+++ vue/afficher_nouvelle_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1> Afficher nouvelle </h1>
+    <?php
+      $nouvelle = $data['nouvelle'];
+      print()
+     ?>
+  </body>
+</html>
