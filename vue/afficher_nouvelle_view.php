d9b1d476210d02a2f46093bccf5ebac22e3ecf6e
ChatonRêveur <vincent.marechal.pro@gmail.com>
2017-10-30 13:04:21 +0100

    ajout de la vue nouvelle en fct de l'id

--- vue/afficher_nouvelle_view.php
+++ vue/afficher_nouvelle_view.php
@@ -7,8 +7,29 @@
   <body>
     <h1> Afficher nouvelle </h1>
     <?php
+      $nouvelles = new nouvelle ();
       $nouvelle = $data['nouvelle'];
-      print()
+      print_r ( "<td><input align='center' type='submit' name='flux' value='retour'></td>" );
+      ?>
+      <table>
+      <tr>
+      <th>le titre</th>
+      <th>la description</th>
+      <th>la date</th>
+      <th>acceder au contenu (site)</th>
+      </tr>
+      <?php
+      	print_r ( "<tr>
+      	<td>{$nouvelles['titre']}</td>
+      	<td>{$nouvelles['description']}</td>
+      	<td>{$nouvelles['date']}</td>
+      	<td><a href={$nouvelles['url']}>{$nouvelles['url']}</a></td>
+      	</tr>" );
+      ?>
+      </table>
+      <?php
+      print_r ( "<td><input align='center' type='submit' name='flux' value='retour'></td>" );
+      ?>
      ?>
   </body>
 </html>
